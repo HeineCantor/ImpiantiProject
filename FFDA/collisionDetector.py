c8b2ddcffa43bfd59efe52d1342f16e45296c1ac
HeineCantor <carmine.palmese@gmail.com>
2024-02-11 17:53:49 +0100

    ffda collision and truncation analysis

---
+++ FFDA/collisionDetector.py
@@ -0,0 +1,18 @@
+from os import listdir
+
+FOLDER_PATH = "/home/heinecantor/Dropbox/UNI/Impianti/Esercitazioni/Homework_FFDA/ffdatoolset/tupling_MercuryErrorLog-200/"
+
+tupleFileList = [f for f in listdir(FOLDER_PATH) if "tuple" in str(f)]
+
+numberOfCollisions = 0
+
+for i in range(len(tupleFileList)):
+    tupleContent = open(FOLDER_PATH + tupleFileList[i]).read().split('\n')
+
+    nodesList = list(set([x.split(' ')[1] for x in tupleContent[:-1]]))
+
+    if(len(nodesList) > 1):
+        print(f"COLLISION: on tuple {i}")
+        numberOfCollisions += 1
+
+print(f"\nTotal number of collisions: {numberOfCollisions}")
---
+++ FFDA/truncationsDetector.py
@@ -0,0 +1,27 @@
+from os import listdir
+
+FOLDER_PATH = "/home/heinecantor/Dropbox/UNI/Impianti/Esercitazioni/Homework_FFDA/ffdatoolset/tupling_MercuryErrorLog-200/"
+
+currentTuple, nextTuple = None, None
+
+tupleFileList = [f for f in listdir(FOLDER_PATH) if "tuple" in str(f)]
+tupleFileList.sort(key=lambda fileName: int(fileName.split('_')[1]))
+
+currentTuple = open(FOLDER_PATH + tupleFileList[0]).read().split('\n')
+nextTuple = open(FOLDER_PATH + tupleFileList[1]).read().split('\n')
+
+numberOfTrunactions = 0
+
+for i in range(1, len(tupleFileList)):
+    lastNodeOfCurrent = currentTuple[-2].split(' ')[1]
+    firstNodeOfNext = nextTuple[0].split(' ')[1]
+
+    if(lastNodeOfCurrent == firstNodeOfNext):
+        print(f"TRUNCATION: {i} VS. {i+1} --- Node: {firstNodeOfNext}")
+        numberOfTrunactions += 1
+
+    currentTuple = nextTuple
+    if(i+1 < len(tupleFileList)):
+        nextTuple = open(FOLDER_PATH + tupleFileList[i+1]).read().split('\n')
+
+print(f"\nTotal number of truncations: {numberOfTrunactions}")
